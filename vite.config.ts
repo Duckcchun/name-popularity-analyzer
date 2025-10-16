e307d900d13f8677d6eecaf7c2f8aa15a77fcb00
duckchun <qasw1733@gmail.com>
2025-10-17 01:16:22 +0900

    Fix: Final base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/name-popularity-analyzer/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
